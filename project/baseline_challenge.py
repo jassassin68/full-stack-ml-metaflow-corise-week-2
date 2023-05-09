8bd81b1788f7ffb9ffafaf3c2d330b8988d6ac6f
Jeffrey Asselin <jasselin@forwardfinancing.com>
2023-05-09 00:47:14 +0000

    dev work on task 3

--- project/baseline_challenge.py
+++ project/baseline_challenge.py
@@ -1,3 +1,4 @@
+
 # TODO: In this cell, write your BaselineChallenge flow in the baseline_challenge.py file.
 
 from metaflow import FlowSpec, step, Flow, current, Parameter, IncludeFile, card, current
@@ -5,12 +6,7 @@ from metaflow.cards import Table, Markdown, Artifact, Image
 import numpy as np 
 from dataclasses import dataclass
 
-def labeling_function(row):
-    if row['rating'] > 4:
-        label = 1
-    else:
-        label = 0
-    return label
+labeling_function = ... # TODO: Define your labeling function here.
 
 @dataclass
 class ModelResult:
@@ -37,8 +33,8 @@ class BaselineChallenge(FlowSpec):
         
         # load dataset packaged with the flow.
         # this technique is convenient when working with small datasets that need to move to remove tasks.
+        df = pd.read_csv(io.StringIO(self.data)) 
         # TODO: load the data. 
-        df = pd.read_csv(io.StringIO(self.data))
         # Look up a few lines to the IncludeFile('data', default='Womens Clothing E-Commerce Reviews.csv'). 
         # You can find documentation on IncludeFile here: https://docs.metaflow.org/scaling/data#data-in-local-files
 
@@ -89,6 +85,7 @@ class BaselineChallenge(FlowSpec):
     def model(self):
 
         # TODO: import your model if it is defined in another file.
+        from model import NbowModel
 
         self._name = "model"
         # NOTE: If you followed the link above to find a custom model implementation, 
@@ -107,8 +104,62 @@ class BaselineChallenge(FlowSpec):
 
         self.next(self.aggregate)
 
+    def add_one(self, rows, result, df):
+        "A helper function to load results."
+        rows.append([
+            Markdown(result.name),
+            Artifact(result.params),
+            Artifact(result.pathspec),
+            Artifact(result.acc),
+            Artifact(result.rocauc)
+        ])
+        df['name'].append(result.name)
+        df['accuracy'].append(result.acc)
+        return rows, df
+
+    @card(type="corise") # TODO: Set your card type to "corise". 
+            # I wonder what other card types there are?
+            # https://docs.metaflow.org/metaflow/visualizing-results
+            # https://github.com/outerbounds/metaflow-card-altair/blob/main/altairflow.py
     @step
     def aggregate(self, inputs):
+
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        from matplotlib import rcParams 
+        rcParams.update({'figure.autolayout': True})
+
+        rows = []
+        violin_plot_df = {'name': [], 'accuracy': []}
+        for task in inputs:
+            if task._name == "model": 
+                for result in task.results:
+                    print(result)
+                    rows, violin_plot_df = self.add_one(rows, result, violin_plot_df)
+            elif task._name == "baseline":
+                print(task.result)
+                rows, violin_plot_df = self.add_one(rows, task.result, violin_plot_df)
+            else:
+                raise ValueError("Unknown task._name type. Cannot parse results.")
+            
+        current.card.append(Markdown("# All models from this flow run"))
+
+        # TODO: Add a Table of the results to your card! 
+        current.card.append(
+            Table(
+                rows, # TODO: What goes here to populate the Table in the card? 
+                headers=["Model name", "Params", "Task pathspec", "Accuracy", "ROCAUC"]
+            )
+        )
+        
+        fig, ax = plt.subplots(1,1)
+        plt.xticks(rotation=40)
+        sns.violinplot(data=violin_plot_df, x="name", y="accuracy", ax=ax)
+        
+        # TODO: Append the matplotlib fig to the card
+        # Docs: https://docs.metaflow.org/metaflow/visualizing-results/easy-custom-reports-with-card-components#showing-plots
+        current.card.append(Image.from_matplotlib(fig))
+
         self.next(self.end)
 
     @step
